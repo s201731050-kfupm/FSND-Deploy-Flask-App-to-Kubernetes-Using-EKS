0ad50258ab7054fd90a955a863832946dd431188
s201731050-kfupm <s201731050@kfupm.edu.sa>
2020-06-11 02:23:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import jwt
 from flask import Flask, jsonify, request, abort
 
 # test
+# test2
 JWT_SECRET = os.environ.get('JWT_SECRET', 'abc123abc1234')
 LOG_LEVEL = os.environ.get('LOG_LEVEL', 'INFO')
 
